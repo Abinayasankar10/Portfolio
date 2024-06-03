5d061282031f8b17192f774f4ca75213e077aa21
Abinayasankar10 <abinayasankar1828@gmail.com>
2024-06-03 14:29:14 +0530

    firts commit

---
+++ src/constants/index.js
@@ -0,0 +1,75 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg";
+
+export const HERO_CONTENT = `I am an enthusiastic full stack developer skilled in building robust and scalable web applications. My expertise spans a wide range of technologies, including HTML, React.js, Tailwind CSS, Node.js, Python, and MySQL. With a meticulous approach and a solid grasp of business goals, I consistently create innovative solutions that foster growth and improve user experiences. Naturally collaborative, I excel in team settings, utilizing strong communication abilities and creative problem-solving skills. Dedicated to continuous improvement, I keep up with emerging trends and technologies, ensuring my skills stay current and adaptable in the fast-changing digital landscape.`;
+
+export const ABOUT_TEXT = `Being a pre-final year engineering student, I am a motivated and adaptable learner with a strong passion for full-stack web development. I have built a robust foundation in React, HTML, CSS, Tailwind CSS, MySQL, and programming languages such as Python, C, and Java. Driven by an unquenchable curiosity to understand the intricacies of technology, I have delved into web development, constantly expanding my knowledge and honing my skills. My journey has been thrilling, filled with challenges that have pushed me to develop efficient and user-centric web applications. I flourish in team environments, where I can leverage my problem-solving skills and learn from seasoned professionals. Beyond coding, I actively seek to explore emerging technologies, maintain physical activity, and contribute to open-source projects, which enriches my learning experience. With a mindset focused on continuous improvement and a genuine passion for the field, I am excited to pursue internships or entry-level positions that will enable me to apply my skills, gain hands-on experience, and further my expertise in full-stack web development.`;
+
+export const EDUCATION = [
+  {
+    year: "2021 - 2025",
+    institute: "R.M.D. Engineering College",
+    description: "Btech Computer Science And Business Systems",
+    percentage: "8.4 Cgpa",
+  },
+  {
+    year: "2019 - 2021",
+    institute: "St.Thomas Group Of Schools",
+    description: "H.S.C" ,
+    percentage: "94.1%",
+  },
+  {
+    year: "2018 - 2019",
+    institute: "Sarathy Saraswathy Vidyalaya Matriculation Hr.Sec School",
+    description: "S.S.L.C.",
+    percentage: "93.2%",
+  },
+];
+export const EXPERIENCES = [
+  {
+    year: "2024 - Present",
+    role: "Web Developer Intern",
+    company: "CognoRise InfoTech",
+    description: `I developed contemporary web applications utilizing React.js to create dynamic user interfaces, Tailwind CSS for streamlined styling, and JavaScript to implement intricate functionality. I consistently explored new tools and methodologies to enhance my skills.`,
+    technologies: ["Javascript", "React.js", "Node.js", "html","Tailwind"],
+  },
+  {
+    year: "2022 - 2023",
+    role: "Database Internship",
+    company: "Madras Fertilizer Limited",
+    description: `I had a valuable experience in the database field. During my internship, the company thoroughly taught me about their database management. I worked on their database systems to retrieve information and developed an application linked to the SQL database.`,
+    technologies: ["HTML", "CSS", "python", "mySQL"],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "youtube clone",
+    image: project1,
+    description:
+      "I created a YouTube clone focused on frontend development. This application includes features like video playback, browsing, searching, and user interactions such as likes, comments, and subscriptions. The design is responsive, providing a seamless experience across all devices.",
+    technologies: ["HTML", "CSS", "javascript","React"],
+  },
+  {
+    title: "Depression detection using ECG Signals",
+    image: project2,
+    description:
+      "I designed a system for depression detection using ECG signals. By analyzing the patterns and anomalies in the ECG data, the system can identify potential indicators of depression. This approach leverages machine learning algorithms to classify ECG signals and distinguish between individuals with and without depression. The system aims to provide an early detection mechanism for depression, allowing for timely intervention and support for those in need.",
+    technologies: ["HTML", "CSS", "CNN Algorithm","Angular","Python", "Firebase"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS", "React", "js"],
+  },
+];
+
+export const CONTACT = {
+  address: "Chennai,TamilNadu ",
+  phoneNo: "988469XXXX",
+  email: "abinayasanka****@gmail.com",
+};
