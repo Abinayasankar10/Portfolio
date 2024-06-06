36604f1b39224321deee529385d2c2b17b8ecb95
Abinayasankar10 <157450158+Abinayasankar10@users.noreply.github.com>
2024-06-06 12:46:50 +0530

    third commit

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -6,10 +6,10 @@ const Education = () => {
     <motion.h2 
      whileInView={{opacity:1,y:0}}
      initial={{opacity:0,y:-100}}
-     transition={{duration:0.5}} className=" bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-5xl tracking-tight text-transparent my-20 text-center text-4xl">Education</motion.h2>
+     transition={{duration:0.5}} className=" bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text  tracking-tight text-transparent my-20 text-center text-5xl">Education</motion.h2>
   <div>
   {EDUCATION.map((education,index)=>(
-        <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
+        <div key={index} className="mb-8 mx-6 flex flex-wrap lg:justify-center">
           <motion.div whileInView={{opacity:1,x:0}}
                initial={{opacity:0,x:-100}}
                transition={{duration:1}} className="w-full lg:w-1/4"> 
