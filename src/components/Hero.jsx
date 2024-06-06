6d1bab97d6f05150dd273c44ff1b7941754087f9
Abinayasankar10 <157450158+Abinayasankar10@users.noreply.github.com>
2024-06-06 12:47:54 +0530

    third commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -27,7 +27,7 @@ const Hero = () => {
                     <motion.p 
                     variants={container(1)}
                     initial="hidden"
-                    animate="visible" className="my-2 max-w-xl py-6 font-light tracking-tighter">{HERO_CONTENT}</motion.p>
+                    animate="visible" className="my-2 mx-6 max-w-xl py-6 font-light tracking-tighter">{HERO_CONTENT}</motion.p>
                 </div>
             </div>
             <div className="w-full lg:w-1/2 lg:p-8">
